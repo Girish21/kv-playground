60ebf3c5c42b27e814abb7eba064ffc96e63ab99
Girish21 <girishkumarv21@gmail.com>
2021-12-10 23:43:55 +0530

    Tailwind V3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  purge: ['./app/**/*.{js,ts,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  mode: 'jit',
+  content: ['./app/**/*.{js,ts,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
